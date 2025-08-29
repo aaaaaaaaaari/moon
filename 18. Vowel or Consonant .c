5f4751c4205fc6083b654796997fea14ab122398
Arifa Misty <arifamisty5@gmail.com>
2025-08-29 20:49:50 +0600

    Create 18. Vowel or Consonant .c

---
+++ 18. Vowel or Consonant .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    ch = tolower(ch);
+
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        printf("%c is a Vowel\n", ch);
+    else
+        printf("%c is a Consonant\n", ch);
+
+    return 0;
+}
